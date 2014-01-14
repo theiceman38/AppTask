306f2301e7fe949ad179b561b9ed8010f34a8de3
theiceman38 <olivier.tavel@me.com>
2014-01-14 09:12:16 +0100

    Initial commit from local git

---
+++ AppTask/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppTask
+//
+//  Created by Olivier Tavel on 14/01/2014.
+//  Copyright (c) 2014 Olivier Tavel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppTask/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AppTask
+//
+//  Created by Olivier Tavel on 14/01/2014.
+//  Copyright (c) 2014 Olivier Tavel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
